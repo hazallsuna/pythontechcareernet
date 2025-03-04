527e8bfa738509803d032dd6dd905d9a857615c1
hazallsuna <hazalsuna8@gmail.com>
2025-03-04 23:07:23 +0300

    ilk

---
+++ day1/baslangic.py
@@ -0,0 +1,14 @@
+#yorum
+"""
+çok yorum
+"""
+name =  "Hazal"
+age = 23
+mezun_mu = True
+sehir = "İstanbul"
+
+"hazal" != "Hazal"
+
+name_surname = "Hazal Suna"
+number2 = 344
+print(name,age,mezun_mu,sehir)
